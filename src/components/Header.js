df64f07eb3c5ea66351ef976214cfd3715b7cc9d
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-03-16 07:07:11 -0500

    routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
-      Header component
+      <Link to="/">Login/Signup</Link>
+      <Link to="/main">Main</Link>
     </div>
   )
 }
